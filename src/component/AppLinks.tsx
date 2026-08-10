1c603c75f286cfec8493d217fb7632c970c9bb84
samuel-samson06 <a.samuelsamson@gmail.com>
2026-08-10 15:15:20 +0100

    feat: reorder reviews for better presentation

--- src/component/AppLinks.tsx
+++ src/component/AppLinks.tsx
@@ -4,7 +4,7 @@ function AppLinks() {
   return (
     <>
     {/* CTA Buttons */}
-      <div className="flex flex-wrap items-center justify-center gap-6 sm:gap-12 mt-2 mb-16 z-20 relative pt-2">
+      <div className="flex flex-wrap items-center justify-center gap-6 sm:gap-12 mt-2 mb-6 md:mb-16 z-20 relative pt-2">
           <Link href="https://apps.apple.com/ng/app/mabket/id6785292328" target="_blank" rel="noopener noreferrer">
             <button className="flex md:text-2xl lg:text-3xl items-center gap-3 rounded-full bg-[var(--color-sapphire)] px-8 py-4 font-display font-bold text-[var(--color-carbon-black)] brutalist-shadow border-2 border-[var(--color-carbon-black)] transition-transform hover:-translate-y-1 active:translate-y-1">
               <img src="/image/Vector.png" alt="" className="h-5 w-5" aria-hidden="true" />
